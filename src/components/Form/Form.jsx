bbdf76ef1dff51a6a63917d7b8dbb44cf278ae9f
Sara Nurestani <saranurestani@gmail.com>
2022-08-16 19:00:20 +0100

    Render form component for submission

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,14 +1,31 @@
 export default function Form() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
   return (
-    <form>
+    <form
+      className="mx-auto w-48 flex-col items-center"
+      onSubmit={handleSubmit}
+    >
       <div>
         <label htmlFor="name" className="sr-only">
           Name
         </label>
-        <input type="text" placeholder="Enter your name" id="name" />
+        <input
+          type="text"
+          placeholder="Enter your name"
+          id="name"
+          className="rounded border pl-2"
+          name="name"
+        />
       </div>
 
-      <button type="submit">Submit</button>
+      <button
+        type="submit"
+        className="mt-4 rounded bg-green-500 px-4 py-2 text-white"
+      >
+        Submit
+      </button>
     </form>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 
 export default function Header({ name }) {
-  return <h1 className="text-3xl">Hello {name}</h1>;
+  return <h1 className="my-8 text-center text-3xl">Hello {name}</h1>;
 }
 
 Header.propTypes = {
